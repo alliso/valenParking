21f0e7be20d0f36b38f024bb536c22d47569491e
Alejandro Lliso <alejandro.lliso.cosin@gmail.com>
2019-03-26 16:35:02 +0100

    Commit random

--- app/src/main/java/com/upv/dadm/valenparking/Fragments/VehicleFragment.java
+++ app/src/main/java/com/upv/dadm/valenparking/Fragments/VehicleFragment.java
@@ -1,16 +1,40 @@
 package com.upv.dadm.valenparking.Fragments;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.upv.dadm.valenparking.R;
 
-public class VehicleFragment extends Fragment {
+public class VehicleFragment extends Fragment implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
 
     View view;
+
+    private String TAG = "Vehicle";
+
+    private GoogleApiClient client;
+    private FusedLocationProviderClient locationProviderClient;
+    private MyLocationCallback callback;
 
     public VehicleFragment(){ }
 
@@ -18,6 +42,98 @@ public class VehicleFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_vehicle, null);
+
+        ImageButton button = view.findViewById(R.id.vehicleSaveButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getLocation();
+            }
+        });
+
+        callback = new MyLocationCallback();
+        client = new GoogleApiClient.Builder(getContext())
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        locationProviderClient = LocationServices.getFusedLocationProviderClient(getContext());
         return view;
+    }
+
+    @Override
+    public void onStart() {
+        client.connect();
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        client.disconnect();
+        locationProviderClient.removeLocationUpdates(callback);
+        super.onStop();
+    }
+
+    public void getLocation() {
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION},
+                    1);
+        } else {
+            locationProviderClient.getLastLocation()
+                    .addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            Log.d(TAG,"Estoy dentro");
+                            if (location != null) {
+                                Log.d(TAG,location.getAltitude() +"ALTITUD");
+                            }
+                        }
+                    }).addOnFailureListener(getActivity(), new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.d(TAG,"FAIIIIIIIIL");
+                }
+            });
+        }
+
+
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        LocationRequest request = new LocationRequest();
+        request.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        request.setInterval(10000);
+        request.setFastestInterval(5000);
+
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            locationProviderClient.requestLocationUpdates(request,callback,null);
+        } else {
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION},
+                    1);
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d(TAG, "Connection Failed");
+    }
+
+
+    private class MyLocationCallback extends LocationCallback {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            super.onLocationResult(locationResult);
+        }
     }
 }
