abef401cd62effca9f2173943303cf535cadf896
Saúl <saul_ibz11@hotmail.com>
2019-03-26 16:18:06 +0100

    Mapa con chincheta añadido

---
+++ app/src/main/java/com/upv/dadm/valenparking/Adapters/CustomInfoWindowGoogleMap.java
@@ -0,0 +1,43 @@
+package com.upv.dadm.valenparking.Adapters;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.Marker;
+import com.upv.dadm.valenparking.Pojo.GoogleMapInfoWindowData;
+import com.upv.dadm.valenparking.R;
+
+public class CustomInfoWindowGoogleMap implements GoogleMap.InfoWindowAdapter {
+
+    private Context context;
+
+    public CustomInfoWindowGoogleMap(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public View getInfoWindow(Marker marker) {
+        return null;
+    }
+
+    @Override
+    public View getInfoContents(Marker marker) {
+        View view = ((Activity)context).getLayoutInflater().inflate(R.layout.fragment_map_marker_info_window, null);
+        TextView name = view.findViewById(R.id.markerInfo_name);
+        TextView address = view.findViewById(R.id.markerInfo_address);
+        TextView freePlaces = view.findViewById(R.id.markerInfo_places);
+        TextView type = view.findViewById(R.id.markerInfo_type);
+
+        GoogleMapInfoWindowData infoData = (GoogleMapInfoWindowData) marker.getTag();
+
+
+        name.setText(infoData.getName());
+        address.setText(infoData.getAddress());
+        freePlaces.setText(infoData.getPlaces());
+        type.setText(infoData.getType());
+        return view;
+    }
+}
--- app/src/main/java/com/upv/dadm/valenparking/Fragments/MapFragment.java
+++ app/src/main/java/com/upv/dadm/valenparking/Fragments/MapFragment.java
@@ -8,37 +8,69 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.upv.dadm.valenparking.Adapters.CustomInfoWindowGoogleMap;
+import com.upv.dadm.valenparking.Pojo.GoogleMapInfoWindowData;
 import com.upv.dadm.valenparking.R;
 
 public class MapFragment extends Fragment implements OnMapReadyCallback {
 
-    View view;
     GoogleMap map;
+    Marker[] markers;
+
+    private SupportMapFragment mapFragment;
 
     public MapFragment(){ }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_map, null);
-        return view;
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        markers = new Marker[10];
     }
 
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-
-        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map_fragment);
-        if (mapFragment != null) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_map, null);
+        if (mapFragment == null) {
+            mapFragment = SupportMapFragment.newInstance();
             mapFragment.getMapAsync(this);
         }
+        getChildFragmentManager().beginTransaction().replace(R.id.map_fragment, mapFragment).commit();
+
+        return view;
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
+        LatLng latLng = new LatLng(39.47805910846728, -0.4070261667406942);
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(latLng);
+        markerOptions.title("Valencia");
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+
+        GoogleMapInfoWindowData infoWindow = new GoogleMapInfoWindowData();
+        infoWindow.setName("Aparcamiento Biopark");
+        infoWindow.setAddress("Av. Pío Baroja");
+        infoWindow.setPlaces("25/256");
+        infoWindow.setType("Tipo C");
+
+        markers[0] = map.addMarker(markerOptions);
+        markers[0].setTag(infoWindow);
+
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 12.0f));
+
+        CustomInfoWindowGoogleMap customInfoWindow = new CustomInfoWindowGoogleMap(getContext());
+        map.setInfoWindowAdapter(customInfoWindow);
     }
 }
