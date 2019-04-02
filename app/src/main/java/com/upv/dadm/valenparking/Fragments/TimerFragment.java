234af43343fe8a6f18b4e19c1812299dd5d8996f
DESKTOP-1FMV25L\David <daalqui@inf.upv.es>
2019-04-02 15:54:34 +0200

    notificación simple en timerfragment

--- app/src/main/java/com/upv/dadm/valenparking/Fragments/TimerFragment.java
+++ app/src/main/java/com/upv/dadm/valenparking/Fragments/TimerFragment.java
@@ -1,8 +1,14 @@
 package com.upv.dadm.valenparking.Fragments;
 
+import android.app.AlarmManager;
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.NotificationCompat;
@@ -13,7 +19,10 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TimePicker;
 
+import com.upv.dadm.valenparking.MainActivity;
 import com.upv.dadm.valenparking.R;
+
+import static android.content.Context.ALARM_SERVICE;
 
 public class TimerFragment extends Fragment {
 
@@ -51,16 +60,50 @@ public class TimerFragment extends Fragment {
 
 
     public void disableChildren() {
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(getContext(), "default")
-                .setSmallIcon(R.drawable.common_google_signin_btn_icon_dark)
-                .setContentTitle("My notification")
-                .setContentText("Much longer text that cannot fit one line...")
-                .setStyle(new NotificationCompat.BigTextStyle()
-                        .bigText("Much longer text that cannot fit one line..."))
-                .setPriority(NotificationCompat.PRIORITY_HIGH);
-        NotificationManager manager = (NotificationManager) getContext().getSystemService(Context.NOTIFICATION_SERVICE);
 
-        manager.notify(0, builder.build());
+        /*
+        Intent i = new Intent(getActivity(), MainActivity.class);
+        PendingIntent pemdingIntent = PendingIntent.getBroadcast(getActivity(),0,i,0);
+
+        AlarmManager am = (AlarmManager) getActivity().getSystemService(Context.ALARM_SERVICE);
+
+        am.setInexactRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() , 8000, pemdingIntent);
+        */
+
+
+        NotificationManager manager = (NotificationManager) getActivity().getSystemService(Context.NOTIFICATION_SERVICE);
+        String NOTIFICATION_CHANNEL_ID = "my_channel_id_01";
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "My Notifications", NotificationManager.IMPORTANCE_DEFAULT);
+
+            // Configure the notification channel.
+            notificationChannel.setDescription("Channel description");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+            notificationChannel.enableVibration(true);
+            manager.createNotificationChannel(notificationChannel);
+        }
+
+        Intent i = new Intent(getActivity(), MainActivity.class);
+        PendingIntent pemdingIntent = PendingIntent.getActivity(getActivity(),0,i,0);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(getActivity(), NOTIFICATION_CHANNEL_ID);
+
+        notificationBuilder.setAutoCancel(true)
+                .setContentIntent(pemdingIntent)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setTicker("Hearty365")
+                //     .setPriority(Notification.PRIORITY_MAX)
+                .setContentTitle("PARKING NOTIFICATION")
+                .setContentText("Te notifico locooooo.")
+                .setContentInfo("Info");
+
+        manager.notify(1, notificationBuilder.build());
+
 
         Log.d(TAG, "AQUIIIIIIIIII");
 
---
+++ app/src/main/java/com/upv/dadm/valenparking/Services/AlarmReceiver.java
@@ -0,0 +1,58 @@
+package com.upv.dadm.valenparking.Services;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+
+import com.upv.dadm.valenparking.MainActivity;
+import com.upv.dadm.valenparking.R;
+
+public class AlarmReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        showNotification(context);
+    }
+
+    public void showNotification(Context context){
+        NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        String NOTIFICATION_CHANNEL_ID = "my_channel_id_01";
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "My Notifications", NotificationManager.IMPORTANCE_DEFAULT);
+
+            // Configure the notification channel.
+            notificationChannel.setDescription("Channel description");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+            notificationChannel.enableVibration(true);
+            manager.createNotificationChannel(notificationChannel);
+        }
+
+        Intent i = new Intent(context, MainActivity.class);
+        PendingIntent pemdingIntent = PendingIntent.getActivity(context,0,i,0);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID);
+
+        notificationBuilder.setAutoCancel(true)
+                .setContentIntent(pemdingIntent)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setTicker("Hearty365")
+                //     .setPriority(Notification.PRIORITY_MAX)
+                .setContentTitle("Default notification")
+                .setContentText("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+                .setContentInfo("Info");
+
+        manager.notify(1, notificationBuilder.build());
+
+    }
+}
