0e16d9a7fe23b9d082d1645b59ef05cec77ce69a
Saúl <saul_ibz11@hotmail.com>
2019-04-04 16:27:03 +0200

    Auto stash before merge of "dev" and "origin/dev"

--- app/src/main/java/com/upv/dadm/valenparking/Fragments/MapFragment.java
+++ app/src/main/java/com/upv/dadm/valenparking/Fragments/MapFragment.java
@@ -3,6 +3,7 @@ package com.upv.dadm.valenparking.Fragments;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Camera;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -10,6 +11,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,19 +19,30 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-//import com.google.android.gms.location.FusedLocationProviderClient;
-//import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.upv.dadm.valenparking.Adapters.CustomInfoWindowGoogleMap;
 import com.upv.dadm.valenparking.Pojo.GoogleMapInfoWindowData;
 import com.upv.dadm.valenparking.R;
+
+import java.util.List;
 
 public class MapFragment extends Fragment implements OnMapReadyCallback,
         GoogleMap.OnMyLocationButtonClickListener,
@@ -42,8 +55,11 @@ public class MapFragment extends Fragment implements OnMapReadyCallback,
 
     private GoogleMap map;
     private GoogleApiClient client;
-    //FusedLocationProviderClient locationProviderClient;
+    //private FusedLocationProviderClient locationProviderClient;
     private SupportMapFragment mapFragment;
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
+
 
     public MapFragment(){ }
 
@@ -51,13 +67,8 @@ public class MapFragment extends Fragment implements OnMapReadyCallback,
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        client = new GoogleApiClient.Builder(getContext())
-                .addConnectionCallbacks(getContext())
-                .addOnConnectionFailedListener(getContext())
-                .addApi(LocationServices.API)
-                .build();
-
-        locationProviderClient = LocationServices.getFusedLocationProviderClient(getContext());
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
     }
 */
     @Override
@@ -65,6 +76,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_map, null);
         if (mapFragment == null) {
+            Log.d("USER_DEBUG", "PIccolo");
             mapFragment = SupportMapFragment.newInstance();
             mapFragment.getMapAsync(this);
         }
@@ -76,25 +88,8 @@ public class MapFragment extends Fragment implements OnMapReadyCallback,
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
-        LatLng latLng = new LatLng(39.47805910846728, -0.4070261667406942);
 
-        MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(latLng);
-        markerOptions.title("Valencia");
-        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
-
-        GoogleMapInfoWindowData infoWindow = new GoogleMapInfoWindowData();
-        infoWindow.setName("Aparcamiento Biopark");
-        infoWindow.setAddress("Av. Pío Baroja");
-        infoWindow.setPlaces("25/256");
-        infoWindow.setType("Tipo C");
-
-        map.addMarker(markerOptions).setTag(infoWindow);
-
-        map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 12.0f));
-
-        CustomInfoWindowGoogleMap customInfoWindow = new CustomInfoWindowGoogleMap(getContext());
-        map.setInfoWindowAdapter(customInfoWindow);
+        addMarkers();
 
         if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             map.setMyLocationEnabled(true);
@@ -102,6 +97,86 @@ public class MapFragment extends Fragment implements OnMapReadyCallback,
             ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                     REQUEST_LOCATION);
         }
+
+        CustomInfoWindowGoogleMap customInfoWindow = new CustomInfoWindowGoogleMap(getContext());
+        map.setInfoWindowAdapter(customInfoWindow);
+    }
+
+    public void addMarkers() {
+        Log.d("USER_DEBUG", currentUser.getDisplayName());
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("parkings");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                LatLngBounds.Builder builder = new LatLngBounds.Builder();
+
+                for(DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+                    String latitud = postSnapshot.child("coordinates").child("lat").getValue().toString();
+                    String longitud = postSnapshot.child("coordinates").child("lon").getValue().toString();
+                    String name = postSnapshot.child("name").getValue().toString();
+                    name = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
+                    String address = postSnapshot.child("address").getValue().toString();
+                    String totalPlaces = postSnapshot.child("total").getValue().toString();
+                    String freePlaces = postSnapshot.child("free").getValue().toString();
+
+
+                    LatLng latLng = new LatLng(Float.valueOf(latitud), Float.valueOf(longitud));
+
+                    MarkerOptions markerOptions = new MarkerOptions();
+                    markerOptions.position(latLng);
+                    markerOptions.title("Valencia");
+
+                    int freePlacesInt = Integer.parseInt(freePlaces);
+                    int totalPlacesInt = Integer.parseInt(totalPlaces);
+                    if (freePlacesInt > totalPlacesInt / 2) {
+                        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+                    } else if (freePlacesInt > 10) {
+                        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW));
+                    } else if (freePlacesInt > 0) {
+                        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE));
+                    } else {
+                        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+                    }
+
+                    GoogleMapInfoWindowData infoWindow = new GoogleMapInfoWindowData();
+                    infoWindow.setName(name);
+                    infoWindow.setAddress(address);
+                    infoWindow.setPlaces(freePlaces + " libres de " + totalPlaces);
+                    infoWindow.setType("Tipo C");
+                    infoWindow.setFavourite(false);
+
+                    map.addMarker(markerOptions).setTag(infoWindow);
+                    builder.include(markerOptions.getPosition());
+                }
+
+                LatLngBounds bounds = builder.build();
+                CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, 0);
+                map.animateCamera(cu);
+
+                map.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+                    @Override
+                    public void onInfoWindowClick(Marker marker) {
+                        GoogleMapInfoWindowData infoWindow = new GoogleMapInfoWindowData();
+                        infoWindow = (GoogleMapInfoWindowData) marker.getTag();
+                        Toast.makeText(getContext(), "Has añadido '" + infoWindow.getName() + "' a favoritos", Toast.LENGTH_SHORT).show();
+                        infoWindow.setFavourite(true);
+                        marker.hideInfoWindow();
+                        marker.showInfoWindow();
+                    }
+                });
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        //map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 12.0f));
     }
 
     /*@Override
@@ -137,5 +212,10 @@ public class MapFragment extends Fragment implements OnMapReadyCallback,
     @Override
     public void onConnectionSuspended(int i) {
 
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
     }
 }
